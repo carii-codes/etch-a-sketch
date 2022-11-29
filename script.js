799fceff56984963c36747c3346b5a93995df57c
Carina Cardozo <cardozo1747@gmail.com>
2022-11-29 15:30:22 -0600

    dom elements

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Set default values 
 const default_color = '#000000'
 const default_btn = 'color' 
 const default_size = '16' 
@@ -6,3 +7,28 @@ let currentColor = default_color
 let currentBtn = default_btn
 let currentSize = default_size
 
+// Functions to keep values relevent 
+    function setNewColor(newColor) {
+        currentColor = newColor 
+    };
+
+    function setNewBtn(newBtn) {
+        activateBtn(newBtn)
+        currentBtn = newBtn 
+    };
+
+    function setNewSize(newSize) {
+        currentSize = newSize
+    };
+
+    // Cach DOM elements 
+    const colorPicker = document.getElementById('colorPicker')
+    const colorBtn = document.getElementById('colorBtn')
+    const rainbowBtn = document.getElementById('rainbowBtn')
+    const erasrerBtn = document.getElementById('eraserBtn')
+    const clearBtn = document.getElementById('clearBtn')
+    const sizeValue = document.getElementById('sizeValue')
+    const sizeSlider = document.getElementById('sizeSlider')
+    const grid = document.getElementById('grid')
+
+    
