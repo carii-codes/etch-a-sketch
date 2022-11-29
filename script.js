811ec31d37220aec3bb6994493eeacbb0ec7fac1
Carina Cardozo <cardozo1747@gmail.com>
2022-11-28 23:15:32 -0600

    sketch files

---
+++ script.js
@@ -0,0 +1,8 @@
+const default_color = '#000000'
+const default_btn = 'color' 
+const default_size = '16' 
+
+let currentColor = default_color
+let currentBtn = default_btn
+let currentSize = default_size
+
